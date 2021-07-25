e9d063d23b48e70be16d6084bb2866514c6f6e4e
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2021-07-25 16:51:37 -0500

    Add separate thread.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use gtk4::{Application, ApplicationWindow, HeaderBar, Button, Orientation, Box, GLArea};
+use futures_util::stream::{StreamExt, StreamFuture};
+use gtk4::prelude::*;
+use gtk4::Label;
+use gtk4::{
+    Application, ApplicationWindow, Box, Button, EventControllerKey, GLArea,
+    HeaderBar, IMContextSimple, InputPurpose, Orientation,
+};
 
 /// An action button.
 ///
 /// Action buttons show up in the window titlebar.  At any given moment, the app
-/// can have up to 8 action buttons, so they should be reserved for the most
+/// can have up to 5 action buttons, so they should be reserved for the most
 /// common actions.
 pub enum Action {
     /// Toggle open/closed vertical tab sidebar
@@ -136,8 +142,8 @@ pub enum Action {
 
     /// Jump to a pre-specified location
     Jump,
-    /// Pin File History (and restore)
-    Pin,
+    /// Tag Version or Webpage (Bookmarks and File History)
+    Tags,
     /// Add an item/page
     Add,
     /// Remove an item/page
@@ -155,6 +161,10 @@ pub enum Action {
     /// Leave fullscreen
     Restore,
 
+    /// Look up something
+    Look,
+    /// Dropdown Menu
+    Menu,
     /// Exit (like close, but doesn't close the program)
     Exit,
 }
@@ -233,7 +243,7 @@ fn gtk_icon(action: &Action) -> &str {
         Action::Gamepad => "input-gaming-symbolic",
 
         Action::Jump => "go-jump-symbolic",
-        Action::Pin => "view-pin-symbolic",
+        Action::Tags => "document-save-as-symbolic",
         Action::Add => "list-add-symbolic",
         Action::Remove => "list-remove-symbolic",
         Action::Share => "send-to-symbolic",
@@ -242,47 +252,67 @@ fn gtk_icon(action: &Action) -> &str {
         Action::Help => "help-browser-symbolic",
         Action::Fullscreen => "view-fullscreen-symbolic",
         Action::Restore => "view-restore-symbolic",
-        
+
+        Action::Look => "go-jump-symbolic",
+        Action::Menu => "view-more-symbolic",
         Action::Exit => "application-exit-symbolic",
     }
 }
 
-/*
+/// Messages received by the client.
+#[derive(Debug)]
+enum Event {
+    /// Keyboard Input
+    Key(u32),
+    /// Text Input
+    Text(char),
+}
+
+/// Message sent by the client.
+enum Message {
+    /// When the client requests the program to exit.
+    Exit,
+}
+
+/// Client State
+struct State<'a> {
+    stream: StreamFuture<flume::r#async::RecvStream<'a, Event>>,
+}
+
+// Exit type for State.
+type Exit = ();
+
+impl<'a> State<'a> {
+    fn event(
+        &mut self,
+        (event, stream): (Option<Event>, flume::r#async::RecvStream<'a, Event>),
+    ) -> std::task::Poll<Exit> {
+        dbg!(event);
+        self.stream = stream.into_future();
+        std::task::Poll::Pending
+    }
+}
+
+async fn start(sender: flume::Sender<Message>, recver: flume::Receiver<Event>) {
+    let mut state = State {
+        stream: recver.stream().into_future(),
+    };
+
+    pasts::Loop::new(&mut state)
+        .when(|s| &mut s.stream, State::event)
+        .await;
+
+    sender.send(Message::Exit).unwrap();
+}
+
 fn main() {
-    let app = Application::builder()
-        .application_id("org.example.HelloWorld")
-        .build();
+    let (send, client_recv) = flume::bounded(1);
+    let (client_send, recv) = flume::bounded(1);
 
-    app.connect_activate(|app| {
-        // We create the main window.
-        let win = ApplicationWindow::builder()
-            .application(app)
-            .default_width(640)
-            .default_height(360)
-            .title("Hello, World!")
-            .build();
-        win.maximize();
-        // win.fullscreen();
-        win.set_titlebar(Some(
-            &HeaderBar::builder()
-                .title("Title")
-                .subtitle("Subtitle")
-                .show_close_button(true)
-                .child(&Button::from_icon_name(Some(gtk_icon(&Action::Pin)), IconSize::Button))
-                .build(),
-        ));
-
-        // Don't forget to make all widgets visible.
-        win.show_all();
+    std::thread::spawn(move || {
+        pasts::block_on(start(client_send, client_recv));
     });
 
-    app.run();
-}*/
-
-use gtk4::prelude::*;
-use gtk4::Label;
-
-fn main() {
     let application = Application::builder()
         .application_id("com.aldaronlau.gtk-test")
         .build();
@@ -293,34 +323,114 @@ fn main() {
             .application(app)
             .default_width(640)
             .default_height(360)
-            .title("Hello, World!")
+            .title("Hello, World")
             .build();
         window.maximize();
-        
+
+        // Specialized keyboard types
+        let input = IMContextSimple::new();
+        input.set_input_purpose(InputPurpose::FreeForm); // text
+
+        // input.set_input_purpose(InputPurpose::Email).build(); // email
+        // input.set_input_purpose(InputPurpose::Url).build(); // url
+        // input.set_input_purpose(InputPurpose::Phone).build(); // phone
+        // input.set_input_purpose(InputPurpose::Digits).build(); // pin
+
         let header = HeaderBar::new();
 
-        let sidebar = Button::from_icon_name(Some(gtk_icon(&Action::Sidebar)));
-        sidebar.connect_clicked(|_| {
-            println!("sidebar!");
-        });
+        let tabs = Button::from_icon_name(Some(gtk_icon(&Action::Sidebar)));
+        header.pack_start(&tabs);
 
+        let sidebar = Button::from_icon_name(Some(gtk_icon(&Action::Backward)));
+        sidebar.connect_clicked(|_| {
+            println!("back!");
+        });
         header.pack_start(&sidebar);
 
-        let menu = Button::from_icon_name(Some(gtk_icon(&Action::Sidebar)));
-        let fullscreen = Button::from_icon_name(Some(gtk_icon(&Action::Fullscreen)));
+        let sidebar = Button::from_icon_name(Some(gtk_icon(&Action::Forward)));
+        sidebar.connect_clicked(|_| {
+            println!("forward!");
+        });
+        header.pack_start(&sidebar);
+
+        let new = Button::from_icon_name(Some(gtk_icon(&Action::New)));
+        header.pack_start(&new);
+
+        let search = Button::from_icon_name(Some(gtk_icon(&Action::Search)));
+        let header2 = header.clone();
+        let search_buffer = gtk4::EntryBuffer::new(Some("https://url.url"));
+        let search_bar = gtk4::Entry::builder()
+            .buffer(&search_buffer)
+            .hexpand(true)
+            //.search_mode_enabled(true)
+            //.show_close_button(true)
+            .build();
+        header.pack_start(&search);
+
+        let menu = Button::from_icon_name(Some(gtk_icon(&Action::Menu)));
+        let tags = Button::from_icon_name(Some(gtk_icon(&Action::Tags)));
+        let sidebar = Button::from_icon_name(Some(gtk_icon(&Action::Sync)));
+        let find = Button::from_icon_name(Some(gtk_icon(&Action::Look)));
 
         header.pack_end(&menu);
-        header.pack_end(&fullscreen);
-        
-        window.set_titlebar(Some(
-            &header
-        ));
+        header.pack_end(&sidebar);
+        header.pack_end(&tags);
+        header.pack_end(&find);
+
+        let title_widget = {
+            let title_widget = Box::builder()
+                .orientation(Orientation::Vertical)
+                .hexpand(true)
+                .valign(gtk4::Align::Center)
+                .build();
+
+            let title = Label::new(Some("Page Title"));
+            title.style_context().add_class("title");
+            let subtitle = Label::new(Some("https://url.url"));
+            subtitle.style_context().add_class("subtitle");
+
+            title_widget.append(&title);
+            title_widget.append(&subtitle);
+
+            title_widget
+        };
+
+        let search_bar2 = search_bar.clone();
+        let header2 = header.clone();
+        let title_widget2 = title_widget.clone();
+        search.connect_clicked(move |_| {
+            header2.set_title_widget::<gtk4::Widget>(None);
+            header2.set_title_widget(Some(&search_bar2));
+            // search_bar.set_focus(true);
+
+            std::thread::sleep(std::time::Duration::from_millis(500));
+
+            header2.set_title_widget::<gtk4::Widget>(None);
+            header2.set_title_widget(Some(&title_widget2));
+        });
+
+        let ec_focus = gtk4::EventControllerFocus::new();
+        let window2 = window.clone();
+        let header2 = header.clone();
+        let title_widget2 = title_widget.clone();
+        ec_focus.connect_leave(move |f| {
+            header2.set_title_widget::<gtk4::Widget>(None);
+            header2.set_title_widget(Some(&title_widget2));
+        });
+        search_bar.add_controller(&ec_focus);
+
+        header.set_title_widget::<gtk4::Widget>(None);
+        header.set_title_widget(Some(&title_widget));
+        header.set_decoration_layout(Some("menu:close"));
+
+        window.set_titlebar(Some(&header));
 
         let column = Box::builder().orientation(Orientation::Vertical).build();
 
         window.set_child(Some(&column));
 
-        let header_fullscreen_internal = HeaderBar::builder().show_title_buttons(false).build();
+        let header_fullscreen_internal =
+            HeaderBar::builder().show_title_buttons(false).build();
         let header_fullscreen = gtk4::Revealer::builder()
             .child(&header_fullscreen_internal)
             .reveal_child(false)
@@ -341,7 +451,7 @@ fn main() {
 
         canvas.connect_render(|canvas, context| {
             dbg!(canvas.width(), canvas.height());
-        
+
             #[link(name = "GLESv2")]
             extern "C" {
                 fn glClearColor(r: f32, g: f32, b: f32, a: f32);
@@ -353,6 +463,21 @@ fn main() {
             }
             gtk4::Inhibit(true)
         });
+
+        let keyboard = EventControllerKey::new();
+        keyboard.connect_modifiers(|controller, modtype| {
+            dbg!(modtype);
+            gtk4::Inhibit(true)
+        });
+        keyboard.connect_key_pressed(|controller, keyval, keycode, state| {
+            dbg!(keyval.to_unicode());
+
+            gtk4::Inhibit(true)
+        });
+        keyboard.connect_key_released(|controller, keyval, keycode, state| {
+            dbg!(keyval, keycode, state);
+        });
+        window.add_controller(&keyboard);
 
         column.prepend(&canvas);
         column.prepend(&header_fullscreen);
@@ -369,13 +494,14 @@ fn main() {
         let win = window.clone();
         let col = column.clone();
         let h_f = header_fullscreen.clone();
-        fullscreen.connect_clicked(move |f| {
+        // FIXME: Enter fullscreen.
+        /*fullscreen.connect_clicked(move |f| {
             h_f.show();
             win.fullscreen();
-        });
+        });*/
 
         window.show();
     });
 
-    application.run();
+    std::process::exit(application.run());
 }
