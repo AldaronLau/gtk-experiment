0cac2028f846291bb1fb40a4a1e28a87e8dc6da7
Jeron Aldaron Lau <aldaronlau@gmail.com>
2022-07-23 16:02:38 -0500

    Render OpenGL texture to GLArea

--- src/gtk.rs
+++ src/gtk.rs
@@ -97,6 +97,66 @@ fn gtk_icon(action: &Action) -> &str {
     }
 }
 
+        use std::os::raw::c_void;
+        #[allow(unused)]
+        pub const GL_ELEMENT_ARRAY_BUFFER: u32 = 0x8893;
+        #[allow(unused)]
+        pub const GL_ARRAY_BUFFER: u32 = 0x8892;
+        #[allow(unused)]
+        pub const GL_DYNAMIC_DRAW: u32 = 0x88E8;
+        #[allow(unused)]
+        pub type GLuint = u32;
+        #[allow(unused)]
+        pub type GLint = i32;
+        #[allow(unused)]
+        pub type GLenum = u32;
+        #[allow(unused)]
+        pub type GLboolean = u8;
+        #[allow(unused)]
+        pub type GLsizei = i32;
+        #[allow(unused)]
+        pub type GLchar = i8;
+        #[allow(unused)]
+        pub type GLbitfield = u32;
+        #[allow(unused)]
+        pub type GLsizeiptr = isize;
+        #[allow(unused)]
+        pub type GLfloat = f32;
+        #[allow(unused)]
+        pub type GLubyte = u8;
+        #[allow(unused)]
+pub const GL_FLOAT: u32 = 0x1406;
+#[allow(unused)]
+pub const GL_TEXTURE_2D: u32 = 0x0DE1;
+#[allow(unused)]
+pub const GL_TEXTURE_MAG_FILTER: u32 = 0x2800;
+#[allow(unused)]
+pub const GL_TEXTURE_MIN_FILTER: u32 = 0x2801;
+#[allow(unused)]
+pub const GL_NEAREST: i32 = 0x2600;
+#[allow(unused)]
+pub const GL_LINEAR: i32 = 0x2601;
+#[allow(unused)]
+pub const GL_LINEAR_MIPMAP_LINEAR: i32 = 0x2703;
+#[allow(unused)]
+pub const GL_NEAREST_MIPMAP_NEAREST: i32 = 0x2700;
+#[allow(unused)]
+pub const GL_NEAREST_MIPMAP_LINEAR: i32 = 0x2702;
+#[allow(unused)]
+pub const GL_RGBA: u32 = 0x1908;
+#[allow(unused)]
+pub const GL_UNSIGNED_BYTE: u32 = 0x1401;
+
+extern "system" {
+    fn glBindTexture(a: GLenum, b: GLuint) -> ();
+    fn glBindBuffer(a: GLenum, b: GLuint) -> ();
+    fn glBufferData(a: GLenum, b: GLsizeiptr, c: *const c_void, d: GLenum) -> ();
+    fn glVertexAttribPointer(a: GLuint, b: GLint, c: GLenum, d: GLboolean, e: GLsizei, f: *const c_void) -> ();
+
+    fn glDrawArrays(a: GLenum, b: GLint, c: GLsizei) -> ();
+    // fn glViewport(a: GLint, b: GLint, c: GLsizei, d: GLsizei) -> ();
+}
+
 pub(super) fn main(send: Channel<crate::Event>, recv: Channel<crate::Message>) {
     let application = Application::builder()
         .application_id("com.aldaronlau.gtk-test")
@@ -186,8 +246,6 @@ pub(super) fn main(send: Channel<crate::Event>, recv: Channel<crate::Message>) {
             header2.set_title_widget(Some(&search_bar2));
             // search_bar.set_focus(true);
 
-            std::thread::sleep(std::time::Duration::from_millis(500));
-
             header2.set_title_widget(Option::<&gtk4::Widget>::None);
             header2.set_title_widget(Some(&title_widget2));
         });
@@ -223,20 +281,210 @@ pub(super) fn main(send: Channel<crate::Event>, recv: Channel<crate::Message>) {
         header_fullscreen_internal.pack_end(&menu);
 
         let canvas = GLArea::builder()
-            /*.auto_render(true)
+            .auto_render(false)
             .has_depth_buffer(false)
+            .hexpand(true)
             .use_es(true)
-            .hexpand(true)*/
             .vexpand(true)
             .build();
 
+        struct State {
+            program: GLuint,
+            position: GLint,
+            texpos: GLint,
+            position_buffer: GLuint,
+            texpos_buffer: GLuint,
+            texture: GLuint,
+        }
+
+        let state: *mut State = std::boxed::Box::leak(std::boxed::Box::new(State {
+            program: 0,
+            position: -1,
+            texpos: -1,
+            position_buffer: 0,
+            texpos_buffer: 0,
+            texture: 0,
+        }));
+
         canvas.connect_realize(move |canvas| {
+            let state = unsafe { &mut *state };
+
             canvas.make_current();
+
+            extern "system" {
+
+                fn glCreateShader(a: GLenum) -> GLuint;
+                fn glShaderSource(a: GLuint, b: GLsizei, c: *const *const GLchar, d: *const GLint) -> ();
+                fn glCompileShader(a: GLuint) -> ();
+                fn glCreateProgram() -> GLuint;
+                fn glAttachShader(a: GLuint, b: GLuint) -> ();
+                fn glLinkProgram(a: GLuint) -> ();
+                fn glDetachShader(a: GLuint, b: GLuint) -> ();
+                fn glUseProgram(a: GLuint) -> ();
+                fn glGetAttribLocation(a: GLuint, b: *const GLchar) -> GLint;
+                fn glEnableVertexAttribArray(a: GLuint) -> ();
+                
+                fn glGenTextures(a: GLsizei, b: *mut GLuint) -> ();
+                fn glTexParameteri(a: GLenum, b: GLenum, c: GLint) -> ();
+                fn glTexImage2D(
+                    a: GLenum,
+                    b: GLint,
+                    c: GLint,
+                    d: GLsizei,
+                    e: GLsizei,
+                    f: GLint,
+                    g: GLenum,
+                    h: GLenum,
+                    i: *const c_void,
+                ) -> ();
+                fn glGenBuffers(a: GLsizei, b: *mut GLuint) -> ();
+            }
+            // Compile shader program for rendering to canvas
+            (state.program, state.position, state.texpos) = unsafe {
+                const SHADER_TEX_VERT: &'static [u8] = b"\
+                    #version 100\n\
+                    precision mediump float;\n\
+                    attribute vec2 position;\n\
+                    attribute vec2 texpos;\n\
+                    varying vec2 texcoord;\n\
+                    void main() {\n\
+                        gl_Position = vec4(position.xy, 0.0, 1.0);\n\
+                        texcoord = texpos;\n\
+                    }";
+                const SHADER_TEX_FRAG: &'static [u8] = b"\
+                    #version 100\n\
+                    precision mediump float;\n\
+                    uniform sampler2D texture;\n\
+                    varying vec2 texcoord;\n\
+                    void main() {\n\
+                        gl_FragColor = texture2D(texture, texcoord);\n\
+                    }";
+                // Compile Vertex Shaders
+                let v_shader = glCreateShader(0x8B31 /*vertex*/);
+                glShaderSource(
+                    v_shader,
+                    1,
+                    [SHADER_TEX_VERT.as_ptr() as *const _].as_ptr(),
+                    [SHADER_TEX_VERT.len() as i32].as_ptr(),
+                );
+                glCompileShader(v_shader);
+                // Compile Fragment Shader
+                let f_shader = glCreateShader(0x8B30 /*fragment*/);
+                glShaderSource(
+                    f_shader,
+                    1,
+                    [SHADER_TEX_FRAG.as_ptr() as *const _].as_ptr(),
+                    [SHADER_TEX_FRAG.len() as i32].as_ptr(),
+                );
+                glCompileShader(f_shader);
+                // Link shaders together.
+                let program = glCreateProgram();
+                glAttachShader(program, v_shader);
+                glAttachShader(program, f_shader);
+                glLinkProgram(program);
+                glDetachShader(program, v_shader);
+                glDetachShader(program, f_shader);
+                glUseProgram(program);
+                // Get Vertex Attributes
+                let position = glGetAttribLocation(program, b"position\0".as_ptr().cast());
+                let texpos = glGetAttribLocation(program, b"texpos\0".as_ptr().cast());
+                // Enable Vertex Attributes
+                glEnableVertexAttribArray(position as u32);
+                glEnableVertexAttribArray(texpos as u32);
+                // 
+                (program, position, texpos)
+            };
+            // Create canvas texture raster
+            state.texture = unsafe {
+                let mut texture = std::mem::MaybeUninit::uninit();
+                glGenTextures(1, texture.as_mut_ptr());
+                let texture = texture.assume_init();
+                glBindTexture(GL_TEXTURE_2D, texture);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+                let pixels = vec![255u8; 640 * 360 * 4];
+                glTexImage2D(
+                    GL_TEXTURE_2D,
+                    0,
+                    GL_RGBA as i32,
+                    640 as i32, // w
+                    360 as i32, // h
+                    0,
+                    GL_RGBA,
+                    GL_UNSIGNED_BYTE,
+                    pixels.as_ptr().cast(),
+                );
+                texture
+            };
+            // Create Position Buffer
+            type VertexList = [[f32; 2]; 6];
+            state.position_buffer = unsafe {
+                let position_c: VertexList = [
+                    [-1.0, 1.0],
+                    [-1.0, -1.0],
+                    [1.0, -1.0],
+                    [1.0, -1.0],
+                    [1.0, 1.0],
+                    [-1.0, 1.0],
+                ];
+                let mut buffer = std::mem::MaybeUninit::uninit();
+                glGenBuffers(1, buffer.as_mut_ptr());
+                let buffer = buffer.assume_init();
+                glBindBuffer(GL_ARRAY_BUFFER, buffer);
+                glBufferData(
+                    GL_ARRAY_BUFFER,
+                    std::mem::size_of::<VertexList>() as isize,
+                    position_c.as_ptr().cast(),
+                    GL_DYNAMIC_DRAW,
+                );
+                glVertexAttribPointer(state.position as GLuint, 2, GL_FLOAT, 0, 0, std::ptr::null());
+                buffer
+            };
+            // Create Texture Coordinates Buffer
+            state.texpos_buffer = unsafe {
+                let texpos_c: VertexList = [
+                    [0.0, 1.0],
+                    [0.0, 0.0],
+                    [1.0, 0.0],
+                    [1.0, 0.0],
+                    [1.0, 1.0],
+                    [0.0, 1.0],
+                ];
+                let mut buffer = std::mem::MaybeUninit::uninit();
+                glGenBuffers(1, buffer.as_mut_ptr());
+                let buffer = buffer.assume_init();
+                glBindBuffer(GL_ARRAY_BUFFER, buffer);
+                glBufferData(
+                    GL_ARRAY_BUFFER,
+                    std::mem::size_of::<VertexList>() as isize,
+                    texpos_c.as_ptr().cast(),
+                    GL_DYNAMIC_DRAW,
+                );
+                glVertexAttribPointer(state.texpos as GLuint, 2, GL_FLOAT, 0, 0, std::ptr::null());
+                buffer
+            };
+        });
+
+        canvas.connect_unrealize(move |canvas| {
+            let state = unsafe { &mut *state };
+            
+            extern "system" {
+                fn glDeleteProgram(a: GLuint) -> ();
+                fn glDeleteBuffers(a: GLsizei, b: *const GLuint) -> ();
+                fn glDeleteTextures(a: GLsizei, b: *const GLuint) -> ();
+            }
+            unsafe {
+                glDeleteProgram(state.program);
+                glDeleteTextures(1, [state.texture].as_ptr());
+                glDeleteBuffers(2, [state.position_buffer, state.texpos_buffer].as_ptr());
+            }
         });
 
         let win = window.clone();
         canvas.connect_render(move |canvas, context| {
-            println!("render");
+            let state = unsafe { &mut *state };
+
+            // println!("render");
             // dbg!(canvas.width(), canvas.height());
 
             #[link(name = "GLESv2")]
@@ -246,8 +494,20 @@ pub(super) fn main(send: Channel<crate::Event>, recv: Channel<crate::Message>) {
                 fn glFlush();
             }
             unsafe {
-                glClearColor(0.1, 0.2, 0.3, 1.0);
+                glClearColor(0.0, 0.0, 0.0, 1.0);
                 glClear(0x00004000);
+                // Bind position_buffer to position
+                glBindBuffer(GL_ARRAY_BUFFER, state.position_buffer);
+                glVertexAttribPointer(state.position as GLuint, 2, GL_FLOAT, 0, 0, std::ptr::null());
+                // Bind texpos_buffer to texpos
+                glBindBuffer(GL_ARRAY_BUFFER, state.texpos_buffer);
+                glVertexAttribPointer(state.texpos as GLuint, 2, GL_FLOAT, 0, 0, std::ptr::null());
+                glBindTexture(GL_TEXTURE_2D, state.texture);
+                glDrawArrays(
+                    /* Triangles */ 0x0004 as GLuint,
+                    /* Start */ 0 as GLint,
+                    /* End */ 6 as GLsizei,
+                );
                 glFlush();
             }
 
@@ -255,8 +515,8 @@ pub(super) fn main(send: Channel<crate::Event>, recv: Channel<crate::Message>) {
         });
 
         canvas.add_tick_callback(|canvas, frame_clock| {
-            let micros = frame_clock.frame_time();
-            dbg!(micros);
+            // let micros = std::time::Duration::from_micros(frame_clock.frame_time());
+            // dbg!(micros);
             canvas.queue_render();
             glib::source::Continue(true) // Keep running
         });
